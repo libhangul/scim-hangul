8ece8bdcb7a994adb307c315d1e87b77d5eb4a87
Choe Hwanjin <choe.hwanjin@gmail.com>
2012-06-06 21:48:53 +0900

    Build against GTK+ 3.0
    The setup tool which has gtk ui code has been updated to be compiled
    against GTK+ 3.0 by using conditional compilation.

--- src/scim_hangul_imengine_setup.cpp
+++ src/scim_hangul_imengine_setup.cpp
@@ -228,15 +228,28 @@ static void
 on_default_combo_box_changed         (GtkComboBox *combobox,
                                       gpointer     user_data);
 
+#if GTK_CHECK_VERSION(2, 12, 0)
+static GtkWidget *
+create_options_page();
+
+static GtkWidget *
+create_keyboard_page();
+#else
 static GtkWidget *
 create_options_page(GtkTooltips *tooltip);
 
 static GtkWidget *
 create_keyboard_page(GtkTooltips *tooltip);
+#endif
 
 // Function implementations.
+#if GTK_CHECK_VERSION(2, 12, 0)
+static GtkWidget *
+create_options_page()
+#else
 static GtkWidget *
 create_options_page(GtkTooltips *tooltips)
+#endif
 {
     GtkWidget *vbox;
     GtkWidget *button;
@@ -246,25 +259,41 @@ create_options_page(GtkTooltips *tooltips)
 
     button = gtk_check_button_new_with_mnemonic (_("_Use ascii input mode"));
     gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+#if GTK_CHECK_VERSION(2, 12, 0)
+    gtk_widget_set_tooltip_text(button,
+                          _("Whether to enable to change the input mode "
+                            "between hangul and ascii mode."));
+#else
     gtk_tooltips_set_tip(tooltips, button,
                           _("Whether to enable to change the input mode "
                             "between hangul and ascii mode."), NULL);
+#endif
     g_signal_connect(G_OBJECT(button), "toggled",
                      G_CALLBACK(on_default_toggle_button_toggled), NULL);
     use_ascii_mode_button = button;
 
     button = gtk_check_button_new_with_mnemonic (_("_Show candidate comment"));
     gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+#if GTK_CHECK_VERSION(2, 12, 0)
+    gtk_widget_set_tooltip_text(button,
+            _("Whether to show the comment of candidates or not."));
+#else
     gtk_tooltips_set_tip(tooltips, button,
                           _("Whether to show the comment of candidates or not."), NULL);
+#endif
     g_signal_connect(G_OBJECT(button), "toggled",
                      G_CALLBACK(on_default_toggle_button_toggled), NULL);
     show_candidate_comment_button = button;
 
     button = gtk_check_button_new_with_mnemonic (_("_Commit by word"));
     gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+#if GTK_CHECK_VERSION(2, 12, 0)
+    gtk_widget_set_tooltip_text(button,
+            _("Whether not to commit until any non-hangul character is inputed."));
+#else
     gtk_tooltips_set_tip(tooltips, button,
                           _("Whether not to commit until any non-hangul character is inputed."), NULL);
+#endif
     g_signal_connect(G_OBJECT(button), "toggled",
                      G_CALLBACK(on_default_toggle_button_toggled), NULL);
     commit_by_word_button = button;
@@ -272,8 +301,13 @@ create_options_page(GtkTooltips *tooltips)
     return vbox;
 }
 
+#if GTK_CHECK_VERSION(2, 12, 0)
+static GtkWidget *
+create_keyboard_page()
+#else
 static GtkWidget *
 create_keyboard_page(GtkTooltips *tooltips)
+#endif
 {
     unsigned int i;
 
@@ -296,13 +330,21 @@ create_keyboard_page(GtkTooltips *tooltips)
     GtkWidget *vbox2 = gtk_vbox_new(FALSE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), vbox2, FALSE, TRUE, 0);
 
+#if GTK_CHECK_VERSION(2, 24, 0)
+    GtkWidget *combo_box = gtk_combo_box_text_new();
+#else
     GtkWidget *combo_box = gtk_combo_box_new_text();
+#endif
     gtk_box_pack_start(GTK_BOX(vbox2), combo_box, FALSE, TRUE, 0);
 
     int n = hangul_ic_get_n_keyboards();
     for (i = 0; i < n; i++) {
 	const char* name = hangul_ic_get_keyboard_name(i);
+#if GTK_CHECK_VERSION(2, 24, 0)
+	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo_box), NULL, name);
+#else
 	gtk_combo_box_append_text(GTK_COMBO_BOX(combo_box), name);
+#endif
     }
     g_signal_connect(G_OBJECT(combo_box), "changed",
                      G_CALLBACK (on_default_combo_box_changed), NULL);
@@ -341,7 +383,7 @@ create_keyboard_page(GtkTooltips *tooltips)
         gtk_table_attach (GTK_TABLE (table), key_bindings[i].entry, 1, 2, i, i+1,
                           (GtkAttachOptions) (GTK_FILL|GTK_EXPAND),
                           (GtkAttachOptions) (GTK_FILL), 4, 4);
-        gtk_entry_set_editable (GTK_ENTRY (key_bindings[i].entry), FALSE);
+        gtk_editable_set_editable (GTK_EDITABLE (key_bindings[i].entry), FALSE);
         gtk_entry_set_text (GTK_ENTRY (key_bindings[i].entry),
                             key_bindings[i].data.c_str());
 
@@ -357,8 +399,13 @@ create_keyboard_page(GtkTooltips *tooltips)
         g_signal_connect(G_OBJECT(key_bindings[i].entry), "changed",
                          G_CALLBACK (on_default_editable_changed), NULL);
 
+#if GTK_CHECK_VERSION(2, 12, 0)
+        gtk_widget_set_tooltip_text(key_bindings[i].entry,
+                              _(key_bindings[i].tooltip));
+#else
         gtk_tooltips_set_tip(tooltips, key_bindings[i].entry,
                               _(key_bindings[i].tooltip), NULL);
+#endif
     }
 
     return vbox1;
@@ -370,20 +417,30 @@ create_setup_window ()
     GtkWidget *notebook;
     GtkWidget *label;
     GtkWidget *page;
-    GtkTooltips *tooltips;
 
-    tooltips = gtk_tooltips_new ();
+#if GTK_CHECK_VERSION(2, 12, 0)
+#else
+    GtkTooltips *tooltips = gtk_tooltips_new ();
+#endif
 
     // Create the Notebook.
     notebook = gtk_notebook_new ();
 
     // Create the first page.
+#if GTK_CHECK_VERSION(2, 12, 0)
+    page = create_keyboard_page();
+#else
     page = create_keyboard_page(tooltips);
+#endif
     label = gtk_label_new (_("Keyboard"));
     gtk_notebook_append_page (GTK_NOTEBOOK (notebook), page, label);
 
     // Create the second page.
+#if GTK_CHECK_VERSION(2, 12, 0)
+    page = create_options_page();
+#else
     page = create_options_page(tooltips);
+#endif
     label = gtk_label_new (_("Options"));
     gtk_notebook_append_page (GTK_NOTEBOOK (notebook), page, label);
 
@@ -525,6 +582,4 @@ on_default_key_selection_clicked (GtkButton *button,
     }
 }
 
-/*
-vi:ts=4:nowrap:expandtab
-*/
+// vi:ts=8:sts=4:sw=4:nowrap:expandtab:
